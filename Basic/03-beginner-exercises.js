82d1948198ead3156edadafa5481be3aef18ff12
vlo-dev <community.vlo@gmail.com>
2025-05-23 17:05:49 -0300

    Resultados exercises

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -8,11 +8,44 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=4733
 // 2. Escribe un comentario en varias líneas
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
-
+let name1 = "valeria"
+let age = 28
+let heingt = 1.57
+let email = "community.vlo@gmail.com"
+let isStudent = true
+let isProfesor = false
+let isBootcamp = true
+let isUniversity = false
+let pet
+let partner = null
+let id = Symbol("vlo-dev")
+let followers = 56461312185431n
 // 4. Imprime por consola el valor de todas las variables
-
+console.log(name1)
+console.log(email)
+console.log(age)
+console.log(heingt)
+console.log(isProfesor)
+console.log(isStudent)
+console.log(isBootcamp)
+console.log(isUniversity)
+console.log (pet)
+console.log(partner)
+console.log(id)
+console.log(followers)
 // 5. Imprime por consola el tipo de todas las variables
-
+console.log(typeof name1)
+console.log(typeof age)
+console.log(typeof email)
+console.log(typeof heingt)
+console.log(typeof isProfesor)
+console.log(typeof isStudent)
+console.log(typeof isBootcamp)
+console.log(typeof isUniversity)
+console.log(typeof pet)
+console.log(typeof partner)
+console.log(typeof id)
+console.log(typeof followers)
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
